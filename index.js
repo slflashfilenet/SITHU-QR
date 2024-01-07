2622293c9d5b07539b9f7a0152b701e70c1792f0
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-07 21:02:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 
 
-
-const inviteLink = 'https://chat.whatsapp.com/IZpUGOxDi9vEogXXyY9Mpi';
+const inviteLink = 'IZpUGOxDi9vEogXXyY9Mpi';
 const pino = require("pino");
 let { toBuffer } = require("qrcode");
 const path = require('path');
 const fs = require("fs-extra");
+const { Client } = require('@open-wa/wa-automate');
 const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
@@ -88,7 +88,8 @@ SESSION-ID ==> ${Scan_Id}
 
           let msgsss = await Smd.sendMessage(user, { text: `SITHUWA-MD;;;${Scan_Id}` });
           await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
-          await Smd.acceptInvite(inviteLink);
+          await client.initialize();
+          await client.acceptInvite(inviteLink);
           await delay(1000);
           try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
           
