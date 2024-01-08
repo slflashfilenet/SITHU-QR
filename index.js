69c6c4cc734debf306fd51ae0bf105e379425956
slflashfilenet <77481921+slflashfilenet@users.noreply.github.com>
2024-01-08 16:27:06 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-
 const express = require("express");
 const app = express();
-
-
-
-
 
 const pino = require("pino");
 let { toBuffer } = require("qrcode");
 const path = require('path');
 const fs = require("fs-extra");
 const { Boom } = require("@hapi/boom");
-const PORT = process.env.PORT ||  5000
-const MESSAGE = process.env.MESSAGE ||  `
+const PORT = process.env.PORT || 5000;
+const MESSAGE = process.env.MESSAGE || `
 ╔════◇
 ║ *🧚‍♂️ THANKS YOU CHOOSE SITHU-MD 🧚‍♂️*
 ║ _You complete first step to making Bot._
@@ -31,120 +26,78 @@ const MESSAGE = process.env.MESSAGE ||  `
 *┋    ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ,*
 *┋    ᴜᴅᴀʏᴀɴɢᴀ ᴘʀᴀᴅᴇᴇᴘ,*
 *┖┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┙*
-`
-
-
-
-
-
-
-
-
-
-
+`;
 
 if (fs.existsSync('./auth_info_baileys')) {
     fs.emptyDirSync(__dirname + '/auth_info_baileys');
-  };
-  
-  app.use("/", async(req, res) => {
+};
 
-  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
-  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-  async function SUHAIL() {
-    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
-    try {
-      let Smd =SuhailWASocket({ 
-        printQRInTerminal: false,
-        logger: pino({ level: "silent" }), 
-        browser: Browsers.macOS("Desktop"),
-        auth: state 
-        });
+app.use("/", async (req, res) => {
+    const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay, DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+    const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
 
+    async function SUHAIL() {
+        const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+        try {
+            let Smd = SuhailWASocket({
+                printQRInTerminal: false,
+                logger: pino({ level: "silent" }),
+                browser: Browsers.macOS("Desktop"),
+                auth: state
+            });
 
-      Smd.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect, qr } = s;
-        if (qr) { res.end(await toBuffer(qr)); }
+            Smd.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect, qr } = s;
+                if (qr) { res.end(await toBuffer(qr)); }
 
+                if (connection == "open") {
+                    await delay(3000);
+                    let user = Smd.user.id;
 
-        if (connection == "open"){
-          await delay(3000);
-          let user = Smd.user.id;
+                    let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json');
+                    var Scan_Id = Buffer.from(CREDS).toString('base64');
 
-
-//===========================================================================================
-//===============================  SESSION ID    ===========================================
-//===========================================================================================
-
-          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
-          var Scan_Id = Buffer.from(CREDS).toString('base64')
-         // res.json({status:true,Scan_Id })
-          console.log(`
+                    console.log(`
 ====================  SESSION ID  ==========================                   
 SESSION-ID ==> ${Scan_Id}
 -------------------   SESSION CLOSED   -----------------------
-`)
+`);
 
+                    let msgsss = await Smd.sendMessage(user, { audio: { url: `file://${'/path/to/your/audio/file.mp3'}` } });
+                    await Smd.sendMessage(user, { text: MESSAGE }, { quoted: msgsss });
+                    await delay(1000);
+                    try { await fs.emptyDirSync(__dirname + '/auth_info_baileys'); } catch (e) { }
+                }
 
-          let msgsss = await Smd.sendMessage(user, { text: `SITHUWA-MD;;;${Scan_Id}` });
-          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
-          await delay(1000);
-          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
+                Smd.ev.on('creds.update', saveCreds);
 
-
+                if (connection === "close") {
+                    let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+                    if (reason === DisconnectReason.connectionClosed) {
+                        console.log("Connection closed!");
+                    } else if (reason === DisconnectReason.connectionLost) {
+                        console.log("Connection Lost from Server!");
+                    } else if (reason === DisconnectReason.restartRequired) {
+                        console.log("Restart Required, Restarting...");
+                        SUHAIL().catch(err => console.log(err));
+                    } else if (reason === DisconnectReason.timedOut) {
+                        console.log("Connection TimedOut!");
+                    } else {
+                        console.log('Connection closed with bot. Please run again.');
+                        console.log(reason);
+                    }
+                }
+            });
+        } catch (err) {
+            console.log(err);
+            await fs.emptyDirSync(__dirname + '/auth_info_baileys');
         }
-
-        Smd.ev.on('creds.update', saveCreds)
-
-        if (connection === "close") {            
-            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-            // console.log("Reason : ",DisconnectReason[reason])
-            if (reason === DisconnectReason.connectionClosed) {
-              console.log("Connection closed!")
-             // SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.connectionLost) {
-                console.log("Connection Lost from Server!")
-            //  SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.restartRequired) {
-                console.log("Restart Required, Restarting...")
-              SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.timedOut) {
-                console.log("Connection TimedOut!")
-             // SUHAIL().catch(err => console.log(err));
-            }  else {
-                console.log('Connection closed with bot. Please run again.');
-                console.log(reason)
-              //process.exit(0)
-            }
-          }
-
-
-
-      });
-    } catch (err) {
-        console.log(err);
-       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
     }
-  }
 
-
-
-
-
-
-
-
-  SUHAIL().catch(async(err) => {
-    console.log(err)
-    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
-
-
-    //// MADE WITH 
-
+    SUHAIL().catch(async (err) => {
+        console.log(err);
+        await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+    });
 });
-
-
-  })
-
 
 app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
